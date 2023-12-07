6a929a15581100c05a8dc375e84c6400c70197b7
reham737 <rehamemad737@gmail.com>
2023-12-07 10:02:09 -0800

    Mycommit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_list - frees a list_t list
+ *
+ * @head: obvious
+ *
+ * Return: 0
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
